2624b903526ba812c63a72faa56096153900791c
gitgitWi <gitgitWi@gmail.com>
2025-09-22 02:02:56 +0900

    refactor(services): add abstracts to llm provider service class

--- services/llm_provider_service.py
+++ services/llm_provider_service.py
@@ -1,16 +1,18 @@
 from langchain.schema import BaseMessage
 from langchain_core.language_models import BaseChatModel
+from abc import ABC, abstractmethod
 
 
-class LlmProviderService:
+class LlmProviderService(ABC):
     model: BaseChatModel
 
     def __init__(self, model_name: str):
         self.model_name = model_name
         self.model = self._create_model()
 
+    @abstractmethod
     def _create_model(self):
-        raise NotImplementedError
+        pass
 
     def infer(self, messages: list[BaseMessage]):
         return self.model.invoke(messages)
